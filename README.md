433d2c11ebb738f5ee626d92966a1639f1fe97dc
DevSecStudio <devsecstudio@gmail.com>
2022-06-09 09:25:33 +0200

    Update bootstrap home page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # boole
 Application pour comparer les produits
+http://localhost:8888/boole/index.php?p=display/website
--- templates/include/header.view.php
+++ templates/include/header.view.php
@@ -1,34 +1,31 @@
-<?php
-echo "
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 
 <head>
 
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <meta name='description' content=''>
-    <meta name='author' content=''>
-    <link rel='shortcut icon' href='assets/img/ico/favicon.ico'>
-    <link rel='apple-touch-icon' sizes='144x144' href='assets/img/ico/apple-touch-icon-144x144.png'>
-    <link rel='apple-touch-icon' sizes='114x114' href='assets/img/ico/apple-touch-icon-114x114.png'>
-    <link rel='apple-touch-icon' sizes='72x72' href='assets/img/ico/apple-touch-icon-72x72.png'>
-    <link rel='apple-touch-icon' href='assets/img/ico/apple-touch-icon-57x57.png'>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/img/ico/favicon.ico">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/ico/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/ico/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/ico/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" href="assets/img/ico/apple-touch-icon-57x57.png">
 
     <title>BOOLE APP</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href='assets/css/bootstrap.min.css' rel='stylesheet'>
-    <link href='assets/css/animate.css' rel='stylesheet'>
-    <link href='assets/css/plugins.css' rel='stylesheet'>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/plugins.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href='assets/css/style.css' rel='stylesheet'>
+    <link href="assets/css/style.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href='assets/font-awesome-4.1.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
-    <link href='assets/css/pe-icons.css' rel='stylesheet'>
+    <link href="assets/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/pe-icons.css" rel="stylesheet">
 
 </head>
-";
